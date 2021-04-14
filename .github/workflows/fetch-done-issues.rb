e560ab77af289176417f75625eee3b7089b9938e
Megan O'Neill <oneill38@github.com>
2021-04-14 12:25:09 -0700

    that was silly

--- .github/workflows/fetch-done-issues.rb
+++ .github/workflows/fetch-done-issues.rb
@@ -15,7 +15,7 @@ projects.each do |project|
   puts "#{project.id}: #{project.name}"
 
   next unless project.name == project_name
-  data[:project_name] = []
+  data[project_name.to_sym] = []
 
   client.project_columns(project.id).each do |column|
     puts "  #{column.id}: #{column.name}"
@@ -30,7 +30,7 @@ projects.each do |project|
       url.slice!("repos/")
 
       puts "    #{card.id}: #{url}"
-      data[:project_name] << url
+      data[project_name.to_sym] << url
     end
   end
 end
