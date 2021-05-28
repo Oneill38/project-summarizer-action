c39f8be0ccc123dcc137ab0679afb86091a032d9
Megan O'Neill <oneill38@github.com>
2021-05-28 10:01:48 -0700

    only record this if updated in the last week

--- .github/workflows/fetch-done-issues.rb
+++ .github/workflows/fetch-done-issues.rb
@@ -25,6 +25,8 @@ nwos.each_with_index do |nwo,idx|
       next unless column.name == 'Done'
 
       client.column_cards(column.id).each do |card|
+        next if card.updated_at < 1.week.ago
+        next if card.archived
         # original url comes like: https://api.github.com/repos/jeffrafter/project-summarizer-action/issues/2
         # we want: https://github.com/jeffrafter/project-summarizer-action/issues/2
         url = card.content_url
